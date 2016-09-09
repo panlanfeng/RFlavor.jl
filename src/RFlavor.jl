544938fa9ecb172852d20dc2a67945e23c6618bc
Lanfeng Pan <panlanfeng@gmail.com>
2016-09-09 15:16:01 -0500

    add rep function

--- src/RFlavor.jl
+++ src/RFlavor.jl
@@ -1,5 +1,7 @@
 module RFlavor
 
 # package code goes here
+import StatsBase:IntegerVector, RealVector, IntegerMatrix, RealMatrix
 
+include("rep.jl")
 end # module
---
+++ src/rep.jl
@@ -0,0 +1,37 @@
+"""
+    rep(x, each, times)
+
+Repeat each elements of `x` for `each` times, and then repeat the whole vector for `times`.
+
+#Arguments
+* `each` can be an integer or a vector of the same length as `x`.
+* `times` is an integer.
+
+#Note
+`rep(1:4, each=1:4)` does not work! Use `rep(1:4, 1:4)` instead.
+#Example
+```
+rep(1:4, 1:4, 2)
+rep(1:4, 2, 2)
+rep(rand(4), each=2, times=2)
+```
+"""
+function rep{T <: Integer}(x::AbstractVector, each::AbstractVector{T}, times::Integer=1)
+    if length(x) != length(each)
+        throw(DimensionMismatch("vector lengths must match"))
+    end
+    res = similar(x, sum(each))
+    i = 1
+    for idx in 1:length(x)
+        tmp = x[idx]
+        for kdx in 1:each[idx]
+            res[i] = tmp
+            i += 1
+        end
+    end
+    return repeat(res, outer=times)
+end
+
+rep(x::AbstractVector, each::Integer, times::Integer=1) = repeat(x; inner=each, outer=times)
+#There seems no way to allow each to be union of vector and integer
+rep{T<:Integer}(x::AbstractVector; each::AbstractVector{T} = ones(Int, x), times::Integer = 1) = rep(x, each, times)
