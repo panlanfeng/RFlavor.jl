02ce13367149bd47fa13fee1670ff2fd08057984
Weicheng-mac <weich@iastate.edu>
2016-09-10 13:54:18 -0500

    findinterval

---
+++ src/findinterval.jl
@@ -0,0 +1,32 @@
+"""
+# Description
+Given a vector of non-decreasing breakpoints in `vec`, find the
+     interval containing each element of `x`. Default is left most interval is closed.
+# Example
+findinterval(0:5, 1:4)
+findinterval(1, 1:2)
+findinterval(1, [0.5, 1, 2])
+"""
+
+function findinterval{T <: Number}(x::AbstractVector{T}, vec::AbstractVector{T})
+    if !issorted(vec)
+        error("Error in findInterval:
+          'vec' must be sorted non-decreasingly.")
+    else
+        out = similar(x, Int64)
+        for i in 1:length(x)
+            out[i] = searchsortedlast(vec, x[i])
+        end
+        return out
+    end
+end
+
+function findinterval(x::Number, vec::AbstractVector)
+    if !issorted(vec)
+        error("Error in findInterval:
+          'vec' must be sorted non-decreasingly.")
+    else
+        return searchsortedlast(vec, x)
+    end
+end
+
