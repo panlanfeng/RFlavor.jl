22b26817ec25677874ecb3d46a11ccc6b5fe5484
Lanfeng Pan <panlanfeng@gmail.com>
2016-09-11 17:28:26 -0500

    typo fix in matrixtest

--- test/matrixtest.jl
+++ test/matrixtest.jl
@@ -1,6 +1,5 @@
 @test typeof(RFlavor.matrix(1, 5)) == Array{Float64,2}
 @test size(RFlavor.matrix(rand(10), 5)) == (5,2)
-@test RFlavor.matrix(1:10, 5) == RFlavor.matrix(1:10, 5, byrow = true)'
+@test RFlavor.matrix(1:10, 5) == RFlavor.matrix(1:10, 2, byrow = true)'
 @test RFlavor.matrix(1:10, ncol = 2) == RFlavor.matrix(1:10, 5)
 @test_throws DimensionMismatch RFlavor.matrix(1:5,2)
-
