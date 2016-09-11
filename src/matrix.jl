497aa3fe7c418d18e6c41fda6b49f8aa041d545b
Weicheng-mac <weich@iastate.edu>
2016-09-11 15:24:05 -0500

    minor change

--- src/matrix.jl
+++ src/matrix.jl
@@ -26,7 +26,7 @@ matrix(1:5, ncol=2) # error
 function matrix(x::AbstractVector, nrow::Integer, ncol::Integer, byrow::Bool = false)
     length(x) % nrow == 0 && length(x) == nrow*ncol || throw(DimensionMismatch("Lenth of `x` is not a multiple of `nrow` and `ncol`."))
     if byrow
-        return reshape(float(x), (Int(nrow), Int(ncol)))'
+        return reshape(float(x), (Int(ncol), Int(nrow)))'
     else
         return reshape(float(x), (Int(nrow), Int(ncol)))
     end
