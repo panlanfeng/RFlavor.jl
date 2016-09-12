da258ede023697f6d847271483006db4d212894c
Weicheng-mac <weich@iastate.edu>
2016-09-12 14:57:30 -0500

    done: matrix all kargs combination

--- src/matrix.jl
+++ src/matrix.jl
@@ -1,49 +1,74 @@
 """
-# Descritpion
+    matrix(x, nrow, ncol, byrow)
+
+## Descritpion
 `matrix` creates a matrix from the given set of values.
 
-
-# Arguments
+## Arguments
 * `x`: a real vector
 * `nrow`: the desired number of rows
 * `ncol`: the desired number of columns
 * `byrow`: logical. If `false` (the default) the matrix is filled by
           columns, otherwise the matrix is filled by rows.
 
-# Example
+## Example
 ```
 matrix(1, 5)
 matrix(1.0, 4, 2)
-matrix(rand(10), 5)
-matrix(1:10, 5)
-matrix(1:10, ncol = 2)
-matrix(1:10, 5, byrow = true)
-matrix(1:5,2) # error
-matrix(1:5, ncol=2) # error
+x = rand(10)
+
+matrix(x)
+matrix(x, 5)
+matrix(x, 5, byrow=true)
+
+matrix(x, ncol = 2)
+matrix(x, ncol = 2, byrow = true)
+
+matrix(x, 5, 2, true)
+matrix(1:5, 2) # dimension does not match error
+
+RFlavor.matrix(1, 4)
+RFlavor.matrix(1, 4, 2)
+RFlavor.matrix(2.0, nrow = 4)
+RFlavor.matrix(2.0, nrow = 4, ncol =2)
 ```
 """
 
 function matrix(x::AbstractVector, nrow::Integer, ncol::Integer, byrow::Bool = false)
     length(x) % nrow == 0 && length(x) == nrow*ncol || throw(DimensionMismatch("Lenth of `x` is not a multiple of `nrow` and `ncol`."))
     if byrow
-        return reshape(x, (Int(ncol), Int(nrow)))'
+        return reshape(x, (ncol, nrow))'
     else
-        return reshape(x, (Int(nrow), Int(ncol)))
+        return reshape(x, (nrow, ncol))
     end
 end
 
-function matrix(x::AbstractVector, nrow::Integer; byrow::Bool = false)
-    length(x) % nrow == 0 || throw(DimensionMismatch("Lenth of `x` is not a multiple of `nrow`."))
-    matrix(x, nrow, Int(length(x)/nrow), byrow)
+function matrix(x::AbstractVector, nrow::Integer, ncol::Integer; byrow::Bool = false)
+    matrix(x, nrow, ncol, byrow)
 end
 
-function matrix(x::AbstractVector; ncol::Integer = 1, byrow::Bool = false)
-    length(x) % ncol == 0 || throw(DimensionMismatch("Lenth of `x` is not a multiple of `ncol`."))
-    matrix(x, Int(length(x)/ncol), ncol, byrow)
+function matrix(x::AbstractVector, nrow::Integer; ncol::Integer = div(length(x), nrow), byrow::Bool = false)
+    matrix(x, nrow, ncol, byrow)
+end
+
+function matrix(x::AbstractVector; kwargs...)
+    kwargs_dict = Dict(kwargs)
+    nrow::Integer = get(kwargs_dict, :nrow, -1)
+    ncol::Integer = get(kwargs_dict, :ncol, -1)
+    byrow::Bool = get(kwargs_dict, :byrow, false)
+    if nrow > 0 && ncol > 0
+        return matrix(x, nrow, ncol, byrow)
+    elseif nrow > 0 && ncol < 0
+        return matrix(x, nrow, div(length(x), nrow),byrow)
+    elseif nrow < 0 && ncol > 0
+        return matrix(x, div(length(x), ncol), ncol, byrow)
+    else
+        return matrix(x, length(x), 1, byrow)
+    end
 end
 
 function matrix(x::Real, nrow::Integer, ncol::Integer = 1)
-    fill(float(x), (nrow, ncol))
+    fill(x, (nrow, ncol))
 end
 
 function matrix(x::Real; nrow::Integer = 1, ncol::Integer = 1)
