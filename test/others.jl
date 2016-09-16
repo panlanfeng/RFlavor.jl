312db7b44fd09641c9d650c1784a2ba0625d3b21
Lanfeng Pan <panlanfeng@gmail.com>
2016-09-16 11:43:53 -0500

    add tests

--- test/others.jl
+++ test/others.jl
@@ -2,9 +2,9 @@
 @test isempty(nothing)
 
 x = seq(1, 100, 0.1);
-@test length(head(x))==6
-@test length(tail(x))==6
+@test length(RFlavor.head(x))==6
+@test length(RFlavor.tail(x))==6
 x = matrix(1:120, 30, 4)
-@test nrow(head(x)) == 6
-@test ncol(head(x)) == 4
-@test size(tail(x)) == (6, 4)
+@test nrow(RFlavor.head(x)) == 6
+@test ncol(RFlavor.head(x)) == 4
+@test size(RFlavor.tail(x)) == (6, 4)
