fee4933895ed901601255d0689db3a00813ab9ed
Lanfeng Pan <panlanfeng@gmail.com>
2016-09-16 15:27:29 -0500

    windows warning

--- test/rangetest.jl
+++ test/rangetest.jl
@@ -1,7 +1,7 @@
 
 srand(2016);
 @test RFlavor.datarange(rand(3,4))==(0.10640104595306332,0.9931674276761062)
-@test RFlavor.datarange([randstring(3) for i in 1:5])==String["pQ9","qpx","5H2","W3V","807"]
+@test setequal(RFlavor.datarange([randstring(3) for i in 1:5]),String["pQ9","qpx","5H2","W3V","807"])
 @test RFlavor.datarange([DateTime(2013,7,i) for i in 1:10]) == (DateTime("2013-07-01T00:00:00"),DateTime("2013-07-10T00:00:00"))
 
 using DataFrames
