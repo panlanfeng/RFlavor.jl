f51c577942e98747654f8f766114a455f76cd6c4
weich <weich@iastate.edu>
2016-09-18 01:06:39 -0500

    0.5 compat

--- test/applytest.jl
+++ test/applytest.jl
@@ -3,7 +3,7 @@ y = list(a = ["a", "b", "c"], b = ["d", "e", "f"])
 @test unlist(RFlavor.lapply(x, mean)) == RFlavor.sapply(x, mean)
 @test length(RFlavor.lapply(x, quantile, collect(1:3)/4)) == 3
 @test size(RFlavor.sapply(x, quantile, collect(1:3)/4)) == (3, 3)
-@test typeof(RFlavor.sapply(y, x->outer(x, x))) == Array{Any, 3}
+@test typeof(RFlavor.sapply(y, x->outer(x, x))) <: Array
 ## need `==` for List object
 # RFlavor.lapply(x, x->outer(x, x)) == RFlavor.sapply(x, x->outer(x, x))
 
